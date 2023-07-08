8b2fa825ca5d9801bba6cc921857dcbfc91887a6
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2023-07-07 22:15:16 -0500

    cree y agregue unos styles, componet, servicio useuario
    servicio usuario solo lo deje creado

--- sistema-examenes-angular-frontend/src/app/app.module.ts
+++ sistema-examenes-angular-frontend/src/app/app.module.ts
@@ -6,16 +6,28 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {MatButtonModule} from "@angular/material/button";
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { SignupComponent } from './page/signup/signup.component';
+import { LoginComponent } from './page/login/login.component';
+import {NgOptimizedImage} from "@angular/common";
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    NgOptimizedImage,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
