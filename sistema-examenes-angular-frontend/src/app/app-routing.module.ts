0c51e2b3b4c79c618afb77854737d32624b36ef9
XSERGIODM <sergiodanilomosqueraquigua@gmail.com>
2023-07-08 02:52:20 -0500

    Modificacion es el css principal, login html, css login, home

--- sistema-examenes-angular-frontend/src/app/app-routing.module.ts
+++ sistema-examenes-angular-frontend/src/app/app-routing.module.ts
@@ -1,16 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {SignupComponent} from "./page/signup/signup.component";
+import {HomeComponent} from "./page/home/home.component";
+import {LoginComponent} from "./page/login/login.component";
 
 
 const routes: Routes = [
-  //{ path: 'empleados', component: ListaEmpleadosComponent }
-  //{path: 'registrarUsuario', component: SignupComponent},
-  //{path: '', redirectTo:'registrarUsuario', pathMatch: 'full'}
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: "full"
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: "full"
+  },
+  {
+    path: 'singup',
+    component: SignupComponent,
+    pathMatch: "full"
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    pathMatch: "full"
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- sistema-examenes-angular-frontend/src/app/app.module.ts
+++ sistema-examenes-angular-frontend/src/app/app.module.ts
@@ -15,6 +15,10 @@ import {MatInputModule} from "@angular/material/input";
 import {FormsModule} from "@angular/forms";
 import { HttpClientModule } from '@angular/common/http';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { HomeComponent } from './page/home/home.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 
 
@@ -23,7 +27,8 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     AppComponent,
     NavbarComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +40,10 @@ import {MatSnackBarModule} from '@angular/material/snack-bar';
     MatInputModule,
     FormsModule,
     HttpClientModule,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
